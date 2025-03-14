268f3f3ecbe349ac0cd09c38b0a777f895ad20ae
EugeneyCh <eugeney@ua.fm>
2025-03-14 09:00:19 +0200

    IV-Day

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,7 +1,15 @@
+import { FC } from "react";
+import { Image } from "../../types";
 import ImageCard from "../ImageCard/ImageCard";
 import s from "./ImageGallery.module.css";
 
-function ImageGallery({ images, openModal }) {
+interface ImageCardProps {
+  images: Image[];
+  openModal: () => void;
+  // (image: Image): void
+}
+
+const ImageGallery: FC<ImageCardProps> = ({ images, openModal }) => {
   return (
     <ul className={s.imageGallery}>
       {images.map((item) => (
@@ -9,7 +17,7 @@ function ImageGallery({ images, openModal }) {
           className={s.imageCard}
           key={item.id}
           id={item.id}
-          // onClick={() => openModal(item)}
+        // onClick={() => openModal(item)}
         >
           <ImageCard item={item} openModal={openModal} />
         </li>
